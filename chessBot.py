ad8a90a5d7f2d541ad2eb12382a64c27b4b9a970
Povilas Kirna <povilas.kirna@gmail.com>
2021-11-20 17:09:09 +0100

    Reworked play function. Working on some bugs regarding turn switching.

--- chessBot.py
+++ chessBot.py
@@ -1,11 +1,41 @@
 from stockfish import Stockfish
+import time
+import chess
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
             
 class Chess:
     def __init__(self):
         self.stockfish = Stockfish()
         self.selected = False
         self.moves = []
-        self.yourTurn = True
+        self.side = self.getSide()
+        self.yourTurn
+        self.settings = {
+            'bestMove' : 1,
+            'advantage' : 1
+        }
                 
     def getSide(self):
         acceptedAnswers = [
@@ -43,40 +73,88 @@ class Chess:
     def makeMove(self, move):
         self.moves.append(move)
         self.stockfish.set_position(self.moves)
+        self.switchTurn()
         
     def help(self):
         print("HELP")
         
     def exit(self):
         exit()
+        
+    def printAdvantage(self):
+        wAdv = 1
+        bAdv = 1
+        evaluation = self.stockfish.get_evaluation()
+        cp = evaluation["value"]
+        currentMoveAdvantage = round((1/(1+10**(-1*cp/4))), 2) #gives advantages after a player did a move for the oponent who will do the move
+        print(cp, currentMoveAdvantage)
+        # if self.side == "WHITE" and self.yourTurn:
+        #     wAdv = 
+
+            
+        # print("White advantage: ", wAdv, " Black advantage: ", bAdv)
+        """
+        possible outcomes:
+        Your turn youre black -> black
+        Your turn youre white -> white
+        Not your turn youre black -> white
+        Not your turn youre white -> black
+        """
+
+
+        # l = 100
+        # # Initial call to print 0% progress
+        # printProgressBar(currentMoveAdvantage*100, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        # for i in range(0, l):
+        #     # Do stuff...
+        #     time.sleep(0.01)
+        #     # Update Progress Bar
+        #     printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+        
+    def play(self):
+        while True:
+            if self.side == "BLACK" and not self.yourTurn:
+                action = input("White's Move / Action: ")
+            elif self.side == "BLACK" and self.yourTurn:
+                if self.settings["bestMove"] == 1:
+                    bestMove = self.getBestMove()
+                    print(bestMove)
+                action = input("Black's Move / Action: ")
+
+            elif self.side == "WHITE" and not self.yourTurn:
+                action = input("Black's Move / Action: ")
+            else:
+                if self.settings["bestMove"] == 1:
+                    bestMove = self.getBestMove()
+                    print(bestMove)
+                action = input("White's Move / Action: ")
+           
+            if self.isMoveLegal(action):
+                self.makeMove(action)
+            else: #if the move is illegal it's most likely a command
+                action = action.upper()
+                if action == "EXIT":
+                    self.exit()
+                elif action == "BOARD":
+                    self.printBoard()
+                elif action == "HELP":
+                    self.help()
+                else:
+                    print("Illegal/unrecognised move or command")
+                # elif action == "SETTINGS" or "SET":
+                #     print("Settings page")
+                #     back = False
+                #     while not back:
+                #         choice = input("Choose setting to change it: ")
+                #         if choice.upper() == "BACK":
+                #             back = True
+
 
 if __name__ == '__main__':
-    chessGame = Chess()
-    chessGame.getSide()
-    while True: 
-        if chessGame.yourTurn:
-            action = input("White's Move / Action: ")
-        else:
-            action = input("Black's Move / Action: ")
-        if chessGame.isMoveLegal(action):
-            move = action
-            if chessGame.yourTurn:
-                chessGame.makeMove(move)
-                chessGame.switchTurn()
-            else:
-                chessGame.makeMove(move)
-                chessGame.switchTurn()
-        else:
-            action = action.upper()
-            if action == "EXIT":
-                chessGame.exit()
-            elif action == "BOARD":
-                chessGame.printBoard()
-            elif action == "HELP":
-                chessGame.help()
-            else:
-                print("Illegal or unrecognised move or command")
-
+    chessGame = Chess()    
+    chessGame.play()
+    
     
     
     """
